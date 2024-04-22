177032b2e7714fa809a8f468aee8f3cbc7adc9f6
lCash133 <lcash@my.chattanoogastate.edu>
2024-04-22 18:15:12 -0400

    v2

---
+++ final/javascript.js
@@ -0,0 +1,21 @@
+var button1 = document.getElementById('button1');
+var button2 = document.getElementById('button2');
+var button3 = document.getElementById('button3');
+var button4 = document.getElementById('button4');
+var button5 = document.getElementById('button5');
+
+button1.addEventListener("click", function() {
+    window.location.href = "./AllTheAnime.html";
+});
+button2.addEventListener("click", function() {
+    window.location.href = "./acceleratorAnime.html";
+});
+button3.addEventListener("click", function() {
+    window.location.href = "./indexAnime.html";
+});
+button4.addEventListener("click", function() {
+    window.location.href = "./RailgunAnime.html";
+});
+button5.addEventListener("click", function() {
+    window.location.href = "./soloLeveling.html";
+});
